a021f428a02161abfb55577438b36550aca4245a
Ivan Vidal <ivanvalmeida.123@gmail.com>
2021-01-21 11:53:06 -0300

    initial logic

--- dashboard/src/app/views/home/home.component.ts
+++ dashboard/src/app/views/home/home.component.ts
@@ -20,24 +20,31 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
 
     this.loginForm = this.fb.group({
-      'num': ['', Validators.required]
-      
+      'num': [[], Validators.required],
+      number: ['', []],
     });
   }
 
-  getValue(){
-this.valor = this.loginForm.value;
 
-this.valor1 = document.createElement('li');
-  var conteudoNovo = document.createTextNode(this.valor.num);
-  this.valor1.appendChild(conteudoNovo); 
+  addOrRemove(e, index = 'false') {
+    e.preventDefault();
 
-console.log(this.valor.num)
+    let numero = this.loginForm.controls.number.value;
+    let lista = this.loginForm.controls.num.value;
+
+    console.log(lista);
+
+    if (index != 'false') {
+      lista.splice(index, 1);
+      this.loginForm.patchValue({ num: lista });
+    } else {
+      if (!numero) return;
+      lista.push(numero);
+      this.loginForm.patchValue({ num: lista, number: '' });
+    }
+
+
   }
-
-
-
-
 
 
 }
